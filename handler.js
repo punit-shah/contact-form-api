0559d5fa32f4cd2be725aec69a4db9231deb0e23
Punit Shah <punit@punitshah.dev>
2019-08-19 16:17:07 +0100

    👌👌👌

--- handler.js
+++ handler.js
@@ -1,18 +1,56 @@
 'use strict';
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const AWS = require('aws-sdk');
+const SES = new AWS.SES();
+const { EMAIL, DOMAIN } = process.env;
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const buildEmailParams = ({ name, email, message }) => {
+  if (!(email && name && message)) {
+    throw Error(
+      "Missing parameters! Make sure to add parameters 'email', 'name', 'message'."
+    );
+  }
+
+  return {
+    Source: EMAIL,
+    Destination: {
+      ToAddresses: [EMAIL],
+    },
+    ReplyToAddresses: [email],
+    Message: {
+      Body: {
+        Text: {
+          Charset: 'UTF-8',
+          Data: `From: ${name} <${email}>
+
+${message}`,
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: `New message from ${DOMAIN}`,
+      },
+    },
+  };
+};
+
+const buildResponse = (statusCode, data) => ({
+  statusCode,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': DOMAIN,
+  },
+  body: JSON.stringify(data),
+});
+
+module.exports.sendEmail = async event => {
+  try {
+    const formData = JSON.parse(event.body);
+    const emailParams = buildEmailParams(formData);
+    const data = await SES.sendEmail(emailParams).promise();
+    return buildResponse(200, data);
+  } catch (error) {
+    console.log(error);
+    return buildResponse(500, error.message);
+  }
 };
